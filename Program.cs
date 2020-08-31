c562e12fef157071578eae954aa08f79f08054af
Lacey Walker <bluelarksong@gmail.com>
2020-08-31 14:42:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chapter8ReadingAndWriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The quick brown");
+            Console.WriteLine("fox jumps");
+            Console.Write("over the");
+            Console.WriteLine("lazy dog");
+            Console.WriteLine("");
+            string input = Console.ReadLine();
+        }
+    }
+}
